f30c7fa997bc32f01e358347a9855c9d67597d6e
Arshiya <arshiyah52@gmail.com>
2023-03-29 13:22:18 +0430

    fix pwa bug

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,4 +12,4 @@ root.render(
 );
 
 
-serviceWorker.register();
+// serviceWorkerRegistration.register();
